5c95d61b486a6459124fb7b441fb6b1e150f56e3
king-ob <oxi8927@rit.edu>
2020-06-07 10:59:59 -0400

    Initial check-in of module AwesomeApp

---
+++ AwesomeApp/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AwesomeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+        int count = 0;
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        
+    }
+}
